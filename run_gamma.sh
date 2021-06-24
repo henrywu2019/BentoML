af60f63c214b01f8398fda7a7fc958abc760a181
Fuheng Wu <fuheng.wu@oracle.com>
2021-06-24 18:49:41 +0000

    run gamma

---
+++ run_gamma.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/home/ubuntu/miniconda3/envs/kappa/bin/python /home/ubuntu/git.repo/kappa/kappa_server.py gamma-service-start --grpc-port 5555 --ui-port 5000 --repo-base-url oci://kappa-bundle --debug
+
+
